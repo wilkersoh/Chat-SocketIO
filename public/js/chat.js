b8442b89888b3ca3deb53347b19eb79425fd22cf
wilkersoh <plgu@hotmail.com>
2019-04-28 21:23:16 +0800

    UI control

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,29 +10,59 @@ const $messages = document.querySelector('#messages');
 //  Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML;
 const locationMessageTemplate = document.querySelector('#location-template').innerHTML;
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
 
-// Options > "?username=yee&room=gs"
+// qs = query string > "?username=yee&room=gs" 
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
+// after added new message scroll it
+const autoscroll = () => {
+    // new msg element
+    const $newMessage = $messages.lastElementChild;
+
+    // get the new message css value
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    // Height of the new message
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+    const visibleHeight = $messages.offsetHeight;
+    const containerHeight = $messages.scrollHeight;
+    // 最上面是0 越下面 数字越大
+    const scrollOffset = $messages.scrollTop + visibleHeight;
+    if(containerHeight - newMessageHeight <= scrollOffset){
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
 
 // render message to html
 socket.on('message', (message) => {
     // mustache 是html里的 script 插件 render html(render的地点，内容)
     const html = Mustache.render(messageTemplate, {
+        username: message.username,
         message: message.text,
         createdAt: moment(message.createdAt).format('hh:mm a')
     });
     $messages.insertAdjacentHTML('beforeend', html);
+    autoscroll();
 })
 
 // render link
 socket.on('locationMessage', (message) => {
-    console.log(message);
     const html = Mustache.render(locationMessageTemplate, {
         username: message.username,
         url: message.url,
         createdAt: moment(message.createdAt).format('hh:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html);
+    autoscroll();
+})
+
+socket.on('roomData', ({room, users}) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.getElementById('sidebar').innerHTML = html;
 })
 
 $messageForm.addEventListener('submit', (e) => {
